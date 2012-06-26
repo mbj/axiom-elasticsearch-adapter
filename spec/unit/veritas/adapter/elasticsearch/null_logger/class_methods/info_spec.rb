43bd32d6750a42ccbe90934760c5406e7e2c2981
Markus Schirp <mbj@seonic.net>
2012-06-26 11:34:36 +0200

    Correct NullLogger#info spec

--- spec/unit/veritas/adapter/elasticsearch/null_logger/class_methods/info_spec.rb
+++ spec/unit/veritas/adapter/elasticsearch/null_logger/class_methods/info_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe Adapter::Elasticsearch::NullLogger,'#warn' do
-  let(:method) { :warn }
+describe Adapter::Elasticsearch::NullLogger,'#info' do
+  let(:method) { :info }
 
   it_should_behave_like 'a null logger method'
 end
