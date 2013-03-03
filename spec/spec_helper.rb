d34054bb3f9ec6ad49e5089698860460deb9e7ff
Markus Schirp <mbj@seonic.net>
2013-03-03 20:20:23 +0100

    Use Devtools.init_{rake_tasks,spec_helper}

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 # encoding: utf-8
 
 require 'veritas-elasticsearch-adapter'
-require 'rspec'
-
 include Veritas
-
-# require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require f }
+require 'devtools'
+Devtools.init_spec_helper
